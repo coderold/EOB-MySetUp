fbf38adbb0aa0cdde2b843d5eb57c9f3220c4d8f
Matthew Molina <mm35molina@gmail.com>
2026-03-14 22:29:14 +0800

    feat: add config

---
+++ config.bat
@@ -0,0 +1,6 @@
+set REPO_PATH=C:\Users\User\Documents\EchoesOfBathala\Echoes-Of-Bathala
+set UNITY_PATH=D:\Unity\Installs\6000.0.60f1\Editor\Unity.exe
+set UNITY_PROJECT=C:\Users\User\Documents\EchoesOfBathala\Echoes-Of-Bathala\unity\Echoes-Of-Bathala
+set DOCKER_PATH=C:\Program Files\Docker\Docker\Docker Desktop.exe
+set GIT_BASH_PATH=C:\Program Files\Git\git-bash.exe
+set VSCODE_PATH=C:\Users\User\AppData\Local\Programs\Microsoft VS Code\Code.exe
--- setup-dev.bat
+++ setup-dev.bat
@@ -1,30 +1,29 @@
 @echo off
 echo Starting Echoes of Bathala Dev Environment...
 
-REM ====== SET REPO PATH ======
-set REPO_PATH=C:\Users\User\Documents\EchoesOfBathala\Echoes-Of-Bathala
+REM Load config
+call config.bat
 
-REM 1. Open Docker Desktop (optional if already running)
-start "" "C:\Program Files\Docker\Docker\Docker Desktop.exe"
+REM 1. Open Docker Desktop
+start "" "%DOCKER_PATH%"
 
-REM Wait 10 seconds for Docker to initialize
 timeout /t 10
 
-REM 2. Start Docker services
-cd /d %REPO_PATH%
+REM 2. Start database
+cd /d "%REPO_PATH%"
 docker compose up -d db
 
-REM 3. Open Git Bash in repo folder
-start "" "C:\Program Files\Git\git-bash.exe" --cd="%REPO_PATH%"
+REM 3. Open Git Bash in repo
+start "" "%GIT_BASH_PATH%" --cd="%REPO_PATH%"
 
-REM 4. Start .NET backend in new terminal
+REM 4. Run backend
 start cmd /k "cd /d %REPO_PATH%\backend\src\GameBackend.API && dotnet watch run"
 
-REM 5. Open Unity Project
-start "" "D:\Unity\Installs\6000.0.60f1\Editor\Unity.exe" -projectPath "%REPO_PATH%\unity\Echoes-Of-Bathala"
+REM 5. Open Unity
+start "" "%UNITY_PATH%" -projectPath "%UNITY_PROJECT%"
 
-REM 6. Open VS Code in repo
-start "" "C:\Users\User\AppData\Local\Programs\Microsoft VS Code\Code.exe" "%REPO_PATH%"
+REM 6. Open VS Code
+start "" "%VSCODE_PATH%" "%REPO_PATH%"
 
-echo Tapos na boss matthew!
+echo Environment Ready!
 pause
